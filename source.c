7bce4f26efbd2dde6e2fd819c2cd4fb51a1b8597
karthikredy7 <98004927+karthikredy7@users.noreply.github.com>
2022-02-08 21:57:17 -0600

    Update source.c

--- source.c
+++ source.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(char argc,char *argv[])
 {
 
 	FILE *F1=NULL,*F2=NULL;
   	
-	if(argc < 3)
+	if(argc < 3) /* minimum 3 argumenrs required*/
 	{
 	  printf("Please Provide the Proper arguments\n");
 		  return 0;
+	}
+	if(strcmp(argv[1],argv[2]) ==0) /*both files are same */
+	{
+	   printf("both files having same name\n");
+	   return 0;
 	}
 	F2 = fopen(argv[2],"r");
 
@@ -25,8 +30,8 @@ int main(char argc,char *argv[])
 	int ss=0;
 	while(!feof(F2))
 		{
-			ss=getline(&line,&size,F2);
-			printf("%d %s %d \n",ss,line,size);
+			ss=getline(&line,&size,F2); /* read the line by line from file */
+		//	printf("%d %s %d \n",ss,line,size);
 			if(ss >0)
 			fwrite(line,ss,1,F1);
 		}
